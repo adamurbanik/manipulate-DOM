902e381182bd570b4eded8161f591fdcbd0c6cc6
Adam Urbanik <adamurbanik@gmail.com>
2016-12-03 00:20:09 +0100

    classes removed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var config = {
-  element: 'div',
+  element: 'section',
   place: 'main-content',
   style: 'color: #0060fc'
 
@@ -11,22 +11,23 @@ var config = {
 var ManipulateDomModule = (function () {
 
   function createElements() {
-    var contentTag = document.getElementsByClassName(config.place)[0];
-
     for (var index = 1; index <= 100; index++) {
       var element = createElement(index);
 
       if (index % 3 === 0) {
         createButton.call(this, element);
       }
-      contentTag.appendChild(element);
+      locateMainTag().appendChild(element);
     }
+  }
+
+  function locateMainTag() {
+    return document.getElementsByClassName(config.place)[0];
   }
 
   function createElement(index) {
     var element = document.createElement(config.element);
     element.innerHTML = 'Element ' + index;
-    element.className = 'element-class'
     return element;
   }
 
@@ -34,7 +35,6 @@ var ManipulateDomModule = (function () {
     var element = document.createElement("input");
     element.type = 'button';
     element.value = 'button';
-    element.className = 'btnDiv';
     element.onclick = function () {
       this.parentNode.setAttribute('style', config.style);
     }
--- tests.js
+++ tests.js
@@ -3,14 +3,14 @@ describe('set directive', function () {
 
   it('should create items', function () {
 
-    var items = document.getElementsByClassName('element-class');
+    var items = document.getElementsByTagName('section');
     expect(items.length).toBe(100);
   });
 
   /* Number of buttons must be the same as number of items divided by 3 */
   it('should items be clickable whenever they have the .btn class', function () {
-    var items = document.getElementsByClassName('element-class');
-    var buttons = document.getElementsByClassName('btnDiv');
+    var items = document.getElementsByTagName('section');
+    var buttons = document.getElementsByTagName('input');
 
     expect(Math.round(items.length / 3)).toBe(buttons.length);
   });
